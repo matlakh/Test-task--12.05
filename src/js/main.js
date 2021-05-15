042fb8c4a0bec48e699b16605eea69fca8c892ff
matlakh <73255302+matlakh@users.noreply.github.com>
2021-05-15 09:44:42 +0300

    footer and header fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,24 @@ $(function () {
     let block_show = null;
 
     function scrollTracking() {
-        let scrollButton = $('.main-section__scroll');
+
+
+
+        $('.main-section__scroll').on('click', function (e) {
+            
+            let link = $(this).attr('href'),
+                el = $(document).find(link);
+            if (el.length > 0) {
+                el = el.eq(0).offset().top;
+                $('html, body').animate({
+                    scrollTop: el + 'px'
+                }, 500, 'linear');
+            }
+            return false;
+        });
         var windowTop = $(window).scrollTop();
         var windowHeight = $(window).height();
-      //  var   = $('.aside__sunflow').offset().top;
+        //  var   = $('.aside__sunflow').offset().top;
         var eh = $('.aside__sunflow').outerHeight();
 
         if (et >= wt && et + eh <= windowHeight + windowTop) {
