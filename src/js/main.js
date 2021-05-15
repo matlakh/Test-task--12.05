95d784d1ce67d61d143ab764a68c30dc5bc6d76c
matlakh <73255302+matlakh@users.noreply.github.com>
2021-05-15 10:56:48 +0300

    fix mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,41 @@
 $(function () {
     let block_show = null;
 
+    const burger = document.querySelector('.hamburger-menu');
+    const menuLinks = document.querySelectorAll('.menu__item a');
+    const mobileMenu = document.querySelector('.menu__box');
+
+    burger.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let pagePos = window.scrollY;
+        document.body.classList.add('is-menu-open');
+        document.setAttribute('data-scroll', pagePos);
+
+        mobileMenu.style.display = 'block';
+    });
+
+    menuLinks.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            let pos = parseInt(document.body.getAttribute('data-scroll'));
+
+            mobileMenu.style.display = 'none';
+
+            document.body.classList.remove('is-menu-open');
+            document.setAttribute('data-scroll', '');
+
+            window.scrollTo(0, pos);
+        });
+    });
+
     function scrollTracking() {
 
 
 
         $('.main-section__scroll').on('click', function (e) {
-            
+
             let link = $(this).attr('href'),
                 el = $(document).find(link);
             if (el.length > 0) {
