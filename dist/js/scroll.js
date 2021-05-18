6b33c19345661affb2bd39107e77240cc7dd7b4e
matlakh <73255302+matlakh@users.noreply.github.com>
2021-05-18 11:03:14 +0300

    animate fix

--- dist/js/scroll.js
+++ dist/js/scroll.js
@@ -1,6 +1,7 @@
 buttonScroll = document.querySelector('.main-section__scroll');
 
 function scrollDown(){
+    console.log(1);
     document.body.scrollTop = 700;
     document.documentElement.scrollTop = 700;
 }
